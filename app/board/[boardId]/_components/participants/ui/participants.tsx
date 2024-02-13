e2cae81a0693285414296c7610a1e471f3b1bb14
your_username <your_email@gmail.com>
2024-02-13 11:05:09 +1100

    feature/room-participants

--- app/board/[boardId]/_components/participants/ui/participants.tsx
+++ app/board/[boardId]/_components/participants/ui/participants.tsx
@@ -1,13 +1,52 @@
+"use client"
+
 import { Skeleton } from "~/components/ui/skeleton"
+import { connectionIdToColor } from "~/lib/utils"
+import { useOthers, useSelf } from "~/liveblocks.config"
+
+import { UserAvatar } from "../../user-avatar"
+
+const MAX_SHOWN_USERS = 2
 
 const Participats = () => {
+  const others = useOthers()
+  const self = useSelf()
+
+  const hasMoreUsers = others.length > MAX_SHOWN_USERS
+
   return (
     <div
       className={
         "absolute right-2 top-2 flex h-12 items-center rounded-md bg-white p-3 shadow-md"
       }
     >
-      List of users
+      <div className={"flex gap-x-2"}>
+        {others.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              borderColor={connectionIdToColor(connectionId)}
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || "A"}
+            />
+          )
+        })}
+        {self && (
+          <UserAvatar
+            borderColor={connectionIdToColor(self.connectionId)}
+            src={self.info?.picture}
+            name={`${self.info?.name} (You)`}
+            fallback={self.info?.name?.[0] || "A"}
+          />
+        )}
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${others.length - MAX_SHOWN_USERS} more`}
+            fallback={`+${others.length - MAX_SHOWN_USERS}`}
+          />
+        )}
+      </div>
     </div>
   )
 }
